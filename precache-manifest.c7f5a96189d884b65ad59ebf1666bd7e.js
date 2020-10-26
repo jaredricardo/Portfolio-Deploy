ffd3dfd5f481e6b89b056b202dd8637bd362d5e5
oyasumi <Jaredricardo94@gmail.com>
2020-10-26 14:34:38 -0400

    Updates

--- precache-manifest.c7f5a96189d884b65ad59ebf1666bd7e.js
+++ precache-manifest.c7f5a96189d884b65ad59ebf1666bd7e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b9dd9488b60ea91ff564456f88eb382",
+    "revision": "856865c799361f060f924a17b6574065",
     "url": "/Portfolio-Deploy/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio-Deploy/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "ce08114bf4784f0c77e3",
-    "url": "/Portfolio-Deploy/static/css/main.d6ecb4f0.chunk.css"
+    "revision": "a72930677f95a8d9d4bc",
+    "url": "/Portfolio-Deploy/static/css/main.d98daf47.chunk.css"
   },
   {
     "revision": "0debdc41c1f5affa9cb4",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio-Deploy/static/js/2.36621ad6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ce08114bf4784f0c77e3",
-    "url": "/Portfolio-Deploy/static/js/main.3bbced58.chunk.js"
+    "revision": "a72930677f95a8d9d4bc",
+    "url": "/Portfolio-Deploy/static/js/main.17ff7d54.chunk.js"
   },
   {
     "revision": "00ad1c76f2b862a9a8fa",
