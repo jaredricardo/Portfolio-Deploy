ad7c82d923ab139097be5f5a7708f6b9a37b5a7a
oyasumi <Jaredricardo94@gmail.com>
2021-03-01 14:38:46 -0500

    Updates

--- precache-manifest.46adafa0e4ed7b7c2644467496634f3d.js
+++ precache-manifest.46adafa0e4ed7b7c2644467496634f3d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea6a2e6420a53153c3a6eb2be8345ca7",
+    "revision": "831eacdd9d91e6bb3ebde704f30e2a83",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "93bc9501277b05d685d4",
+    "revision": "e7e8ffbfcc89fb871dee",
     "url": "/static/css/main.18c2faaa.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.eed20247.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "93bc9501277b05d685d4",
-    "url": "/static/js/main.3608fcaf.chunk.js"
+    "revision": "e7e8ffbfcc89fb871dee",
+    "url": "/static/js/main.8d273397.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
@@ -40,8 +40,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/RobotoMono-VariableFont_wght.9e06bf8e.ttf"
   },
   {
-    "revision": "7815df71258e3cdbb32de8c1b906620a",
-    "url": "/static/media/resume.7815df71.pdf"
+    "revision": "28487d946f674df63f476a3acbd80975",
+    "url": "/static/media/resume.28487d94.pdf"
   },
   {
     "revision": "90e97c3c5787413cd4e91392dca06364",
