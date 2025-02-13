e7c1d6785222940768fb5ae468bce5b48e59765f
jaredricardo <jaredricardo94@gmail.com>
2025-02-13 13:40:05 +0700

    2025 swap in

---
+++ precache-manifest.1f8064807569e670cb8328b597ccd9c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b0948642f853087caed5846cf89f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a985e6d4985e8d80996",
+    "url": "/static/css/2.46b578d6.chunk.css"
+  },
+  {
+    "revision": "7613c68bdf1cab54ab8c",
+    "url": "/static/css/main.1011813b.chunk.css"
+  },
+  {
+    "revision": "5a985e6d4985e8d80996",
+    "url": "/static/js/2.2ca4cc67.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.2ca4cc67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7613c68bdf1cab54ab8c",
+    "url": "/static/js/main.aaa05326.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d7a1aca480ebfe8abace348eeb2779ba",
+    "url": "/static/media/CV2020.d7a1aca4.pdf"
+  },
+  {
+    "revision": "3701d12f4e47a6214ef41b3273b5de5a",
+    "url": "/static/media/Inconsolata-VariableFont_wdth,wght.3701d12f.ttf"
+  },
+  {
+    "revision": "9e06bf8e4155ad3a942a9ff38f59fbc4",
+    "url": "/static/media/RobotoMono-VariableFont_wght.9e06bf8e.ttf"
+  },
+  {
+    "revision": "6aabb553febe60ac0291dab64bdd6028",
+    "url": "/static/media/resume.6aabb553.pdf"
+  },
+  {
+    "revision": "1dd0140398c011cb41658b6d90d412a9",
+    "url": "/static/media/selfie.1dd01403.jpeg"
+  }
+]);
